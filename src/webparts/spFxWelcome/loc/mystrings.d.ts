16ec0510c0a16b0a48d8dcbee65ee1081dcd28c8
Hello <hello@mustangsky.com>
2021-07-16 20:05:04 -0500

    Initial Commit

---
+++ src/webparts/spFxWelcome/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpFxWelcomeWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SpFxWelcomeWebPartStrings' {
+  const strings: ISpFxWelcomeWebPartStrings;
+  export = strings;
+}
